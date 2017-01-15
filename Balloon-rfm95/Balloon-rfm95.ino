763c1ae248b3b8aecb25a7e3b4ec660af2788662
Dennis Ruigrok <dennis.ruigrok@gmail.com>
2017-01-15 01:27:15 +0100

    Why -1!

--- Balloon-rfm95/Balloon-rfm95.ino
+++ Balloon-rfm95/Balloon-rfm95.ino
@@ -144,7 +144,7 @@ void do_send(osjob_t* j){
         Serial.println(F("OP_TXRXPEND, not sending"));
     } else {
         // Prepare upstream data transmission at the next possible time.
-        LMIC_setTxData2(1, mydata, sizeof(mydata)-1, 0);
+        LMIC_setTxData2(1, mydata, sizeof(mydata), 0);
         Serial.println(F("Packet queued"));
     }
     // Next TX is scheduled after TX_COMPLETE event.
